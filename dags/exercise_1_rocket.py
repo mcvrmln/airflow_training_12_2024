a14118a3b8474e2ddb75fb113bcd405807349d3b
Maarten Vermeulen <103961469+mcvrmln@users.noreply.github.com>
2024-12-16 10:37:49 +0000

    Exercise 1

---
+++ dags/exercise_1_rocket.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="Launch a Rocket",
+    start_date=datetime.now() - timedelta(days=14),
+    description="Launch a rocket DAG",
+    schedule="@daily",
+):
+    procure_rocket_material = EmptyOperator(task_id="procure_rocket_material")
+    procure_fuel = EmptyOperator(task_id="procure_fuel")
+    build_stage_1 = EmptyOperator(task_id="build_stage_1")
+    build_stage_2 = EmptyOperator(task_id="build_stage_2")
+    build_stage_3 = EmptyOperator(task_id="build_stage_3")
+    launch = EmptyOperator(task_id="launch")
+
+procure_rocket_material >> [build_stage_1, build_stage_2, build_stage_3] >> launch
+procure_fuel >> build_stage_3
+    
