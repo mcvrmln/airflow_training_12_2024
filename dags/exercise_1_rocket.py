6be3059895ec477a77d9b2f130a0d235ff475d57
Maarten Vermeulen <103961469+mcvrmln@users.noreply.github.com>
2024-12-16 12:29:08 +0000

    Change schedule

--- dags/exercise_1_rocket.py
+++ dags/exercise_1_rocket.py
@@ -6,8 +6,8 @@ with DAG(
     dag_id="launch_a_rocket",
     start_date=datetime.now() - timedelta(days=90),
     description="Launch a rocket DAG",
-    # schedule="45 13 * * 1,3,5",
-    schedule=timedelta(days=3)
+    schedule="45 13 * * 1,3,5",
+    # schedule=timedelta(days=3)
 ):
     procure_rocket_material = EmptyOperator(task_id="procure_rocket_material")
     procure_fuel = EmptyOperator(task_id="procure_fuel")
@@ -15,7 +15,8 @@ with DAG(
     build_stage_2 = EmptyOperator(task_id="build_stage_2")
     build_stage_3 = EmptyOperator(task_id="build_stage_3")
     launch = EmptyOperator(task_id="launch")
+    return_home = EmptyOperator(task_id="return_home")
 
 procure_rocket_material >> [build_stage_1, build_stage_2, build_stage_3] >> launch
 procure_fuel >> build_stage_3
-    
+launch >> return_home
