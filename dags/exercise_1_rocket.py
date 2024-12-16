e2aaa9982f53e9465b11373ea71ef2e3bddfb2bc
Maarten Vermeulen <103961469+mcvrmln@users.noreply.github.com>
2024-12-16 12:25:02 +0000

    WIP

--- dags/exercise_1_rocket.py
+++ dags/exercise_1_rocket.py
@@ -3,8 +3,8 @@ from airflow.models import DAG
 from airflow.operators.empty import EmptyOperator
 
 with DAG(
-    dag_id="launch_a_rocket_cron",
-    start_date=datetime.now() - timedelta(days=14),
+    dag_id="launch_a_rocket",
+    start_date=datetime.now() - timedelta(days=90),
     description="Launch a rocket DAG",
     # schedule="45 13 * * 1,3,5",
     schedule=timedelta(days=3)
